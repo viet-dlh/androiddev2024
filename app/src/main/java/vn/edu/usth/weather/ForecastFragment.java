bed7ec63105f4dce16959068095b94db27d18332
hoangviet <kinkinkin229@gmail.com>
2024-08-28 15:54:59 +0700

    practical 3

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.Gravity;
 
 public class ForecastFragment extends Fragment {
 
@@ -22,8 +26,33 @@ public class ForecastFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(Color.parseColor("#20FF0000"));
-        return view;
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        ));
+        linearLayout.setBackgroundColor(Color.parseColor("#20FF0000"));
+        TextView textView = new TextView(getContext());
+        textView.setText("Thursday");
+        textView.setTextColor(Color.BLACK);
+        textView.setTextSize(24);
+        LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        textParams.gravity = Gravity.CENTER;
+
+        linearLayout.addView(textView, textParams);
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(R.drawable.storm);
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        imageParams.gravity = Gravity.CENTER;
+
+        linearLayout.addView(imageView, imageParams);
+        return linearLayout;
     }
 }
