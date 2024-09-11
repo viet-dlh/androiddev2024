0ec8668164d677f9dd75701fbea8981d70d04826
hoangviet <kinkinkin229@gmail.com>
2024-09-11 15:46:56 +0700

    practical 5

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,20 +1,13 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
-import android.graphics.Color;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.view.Gravity;
 
 public class ForecastFragment extends Fragment {
-
     public ForecastFragment() {
         // Required empty public constructor
     }
@@ -22,36 +15,8 @@ public class ForecastFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        LinearLayout linearLayout = new LinearLayout(getContext());
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-        ));
-        linearLayout.setBackgroundColor(Color.parseColor("#20FF0000"));
-        TextView textView = new TextView(getContext());
-        textView.setText("Thursday");
-        textView.setTextColor(Color.BLACK);
-        textView.setTextSize(24);
-        LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        textParams.gravity = Gravity.CENTER;
-
-        linearLayout.addView(textView, textParams);
-        ImageView imageView = new ImageView(getContext());
-        imageView.setImageResource(R.drawable.cloudy);
-        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        );
-        imageParams.gravity = Gravity.CENTER;
-
-        linearLayout.addView(imageView, imageParams);
-        return linearLayout;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_forecast,container,false);
+        }
     }
-}
--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -2,19 +2,27 @@ package vn.edu.usth.weather;
 
 import android.os.Bundle;
 import android.util.Log;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 public class WeatherActivity extends AppCompatActivity {
-    private static final String TAG = "WeatherActivity";
-
+    public static final String TAG = "Weather";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         Log.i(TAG, "ON_CREATE");
-        ForecastFragment forecastFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.fl1, forecastFragment).commit();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainerView,ForecastFragment.class,null)
+                .commit();
     }
 
     @Override
@@ -22,25 +30,21 @@ public class WeatherActivity extends AppCompatActivity {
         super.onStart();
         Log.i(TAG, "ON_START");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "ON_RESUME");
     }
-
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "ON_PAUSE");
     }
-
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "ON_STOP");
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
