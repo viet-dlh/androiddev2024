b34f5201752e6f136af0bc7e0a1de4d81e364ba4
hoangviet <kinkinkin229@gmail.com>
2024-09-13 14:15:21 +0700

    practical 6

---
+++ app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -0,0 +1,17 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+    public WeatherFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
