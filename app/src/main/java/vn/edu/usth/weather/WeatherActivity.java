b78501038790a7fab6d4b6354fbcbacd9f713cbc
hoangviet <kinkinkin229@gmail.com>
2024-09-13 22:35:28 +0700

    practical 8

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -6,23 +6,29 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String TAG = "Weather";
     private ViewPager2 viewpager2;
-
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         viewpager2 = findViewById(R.id.vp1);
+        tabLayout = findViewById(R.id.tabLayout);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
         viewPagerAdapter.addFragment(new WeatherAndForecastFragment(),"Paris");
         viewPagerAdapter.addFragment(new WeatherAndForecastFragment(),"Tokyo");
         viewPagerAdapter.addFragment(new WeatherAndForecastFragment(),"London");
         viewpager2.setAdapter(viewPagerAdapter);
 
-        Log.i(TAG, "ON_CREATE");
+        new TabLayoutMediator(tabLayout, viewpager2, (tab, position) -> {
+            tab.setText(viewPagerAdapter.getTitle(position));
+        }).attach();
         Log.i(TAG, "ON_CREATE");
     }
 
